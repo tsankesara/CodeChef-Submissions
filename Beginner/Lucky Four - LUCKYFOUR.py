7aa1048e170c101b3d87244431148136af9fd23f
Teerth Sankesara <50574830+tsankesara@users.noreply.github.com>
2020-05-06 16:18:17 +0530

    https://www.codechef.com/problems/LUCKFOUR

---
+++ Beginner/Lucky Four - LUCKYFOUR.py
@@ -0,0 +1,19 @@
+
+#https://www.codechef.com/problems/LUCKFOUR
+test = int(input())
+ans_list = []
+count =0
+for x in range(test):
+    
+    new_in = int(input())
+    new_in = int(new_in)
+    while int(new_in) != 0:
+        out = int(new_in) % 10
+        new_in = int(new_in / 10)
+        if out == 4:
+            count = count + 1
+    ans_list.insert(x, count)
+    count = 0
+x = 0
+for x in range(len(ans_list)):
+    print(ans_list[x])
