7652188eb202cd5cdba7a63d4fde3ecceaa59079
Teerth Sankesara <50574830+tsankesara@users.noreply.github.com>
2020-05-06 11:40:45 +0530

    Chef and Bored Games
    Submission Profile:
    ID => 32760086
    User => chefteerth
    Time => 0.05
    Mem => 87.1
    Lang => PyPy3

---
+++ Beginner/PCJ18B.py
@@ -0,0 +1,34 @@
+# 32760086	26 sec ago	chefteerth		0.05	87.1M	PYPY3
+# https://www.codechef.com/problems/PCJ18B
+# Problem Name: Chef and Bored Games
+# Problem Code: PCJ18B
+# Programming Lang: Python3
+
+
+
+def square(n): 
+  
+    j = 0; 
+  
+    # For all odd values of i 
+    for i in range(1, n + 1, 2): 
+  
+        # Add the count of possible 
+        # squares of length i 
+        k = n - i + 1; 
+        j += (k * k); 
+  
+    # Return the required count
+    j = int(j) 
+    return j; 
+  
+test_cases = int(input())
+test_list = []
+counter = 0
+while test_cases > counter:
+    N = int(input())
+    temp = square(N)
+    test_list.insert(counter, temp)
+    counter = counter + 1
+for x in range(len(test_list)):
+    print(test_list[x])
