ed9e36f5cfa8f82ea35149ba29c494415c316634
tsankesara <teerthsankesara@gmail.com>
2020-06-02 13:44:06 +0530

    Just started..

--- Beginner/Entrance Exam - ENTEXAM.py
+++ Beginner/Entrance Exam - ENTEXAM.py
@@ -1,5 +1,10 @@
 #Username: chefteerth ## https://www.codechef.com/users/chefteerth
+#Question URL: https://www.codechef.com/problems/ENTEXAM
 # Problem Name: Entrance Exam
 # Problem Code: ENTEXAM
 # Programming Lang: Python3
 
+test = int(input())
+while test > 0:
+    num_of_std, seats, no_of_exam, score = map(int, input().split())
+    
