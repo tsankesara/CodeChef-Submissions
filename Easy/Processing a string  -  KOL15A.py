6a91e96e4fe9c5f85459f207e316a0bb6ccb5869
tsankesara <teerthsankesara@gmail.com>
2020-06-03 15:08:29 +0530

    Processing string sol

--- Easy/Processing a string  -  KOL15A.py
+++ Easy/Processing a string  -  KOL15A.py
@@ -4,5 +4,21 @@
 # Problem Code: KOL15A
 # Programming Lang: Python3
 
-
-
+test = int(input())
+i = 0
+ans = []
+while test > 0:
+    temp = 0
+    input_str = input()
+    lenght = len(input_str)
+    for x in range(lenght):
+        if input_str[x] in "1234567890":
+            num = int(input_str[x])
+            temp = temp + num
+    ans.append(temp)
+    test = test -1
+    
+for x in range(len(ans)):
+    print(ans[x])
+            
+## TRY if x in "123456789"
